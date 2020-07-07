ad118d6ba9ec0f4554149e19ae8722b1197ce22c
MOHOMMED MOTAHIR ALAM <mdmotahiralam@gmail.com>
2020-07-07 15:01:10 +0530

    Final

--- django/mart/product/models.py
+++ django/mart/product/models.py
@@ -21,10 +21,10 @@ class Product(models.Model):
     description=models.TextField()
     information=models.TextField()
     unite_price=models.DecimalField(max_digits=8, decimal_places=2)
-    image=models.ImageField(upload_to='product/',blank=True)
-    image2=models.ImageField(upload_to='product/',blank=True)
-    image3=models.ImageField(upload_to='product/',blank=True)
-    image4=models.ImageField(upload_to='product/',blank=True)
+    front_image=models.ImageField(upload_to='product/')
+    back_image=models.ImageField(upload_to='product/')
+    right_image=models.ImageField(upload_to='product/')
+    left_image=models.ImageField(upload_to='product/')
     stock=models.IntegerField()
     discount=models.FloatField(blank=True)
     category=models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -38,23 +38,32 @@ class Product(models.Model):
         super().save(*args,**kwargs)
 
 
-# class Item(models.Model):
-#     product=models.OneToOneField(Product,on_delete=models.CASCADE)
-#     quantity=models.PositiveIntegerField()
-#     price=models.DecimalField(max_digits=8, decimal_places=2,blank=True)
+class Cart(models.Model):
+    user=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
+    item=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.IntegerField(default=1)
+    total=models.DecimalField(max_digits=8, decimal_places=2,blank=True)
+    created=models.DateTimeField(auto_now_add=True)
 
-#     def save(self,*args,**kwargs):
-#         self.price=self.product.unite_price*self.quantity
-#         super().save(*args,**kwargs)
+    def __str__(self):
+        return f'{self.quantity} of {self.item.name}'
 
-# class Cart(models.Model):
-#     user=models.OneToOneField(get_user_model(),on_delete=models.CASCADE)
-#     is_orderd=models.BooleanField()
-#     Item=models.ManyToManyField(Item)
-#     total_price=models.DecimalField(max_digits=8, decimal_places=2)
+    def save(self,*args,**kwargs):
+        self.total = self.item.unite_price * self.quantity
+        super().save(*args,**kwargs)
 
+
+class Order(models.Model):
+    orderitems=models.ManyToManyField(Cart)
+    user=models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    is_order=models.BooleanField(default=False)
+    order_date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
     
-# class Order(models.Model):
-#     user=models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
-#     cart=models.OneToOneField(Cart,on_delete=models.CASCADE)
-#     date_created=models.DateTimeField(auto_now_add=True)
+    def get_total_price(self):
+        total=0
+        for i in self.orderitems.all():
+            total+=i.total
+        return total
